8789a3df318bdb83e59dc28b2516326148043282
海老名彩希 <ebinamasato@MacBook-Air-3.local>
2026-02-19 12:14:49 +0900

    ワーニング表示

--- src/pages/Active.vue
+++ src/pages/Active.vue
@@ -9,7 +9,7 @@
     }
   })
 
-  const emit = defineEmits(['add','toggle','remove'])
+  const emit = defineEmits(['toggle','remove'])
 
   const activeTodos = computed(() => {
     return (props.todos || []).filter((todo) => !todo.completed)
